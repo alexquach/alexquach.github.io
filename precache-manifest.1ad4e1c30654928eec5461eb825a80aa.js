a5d54b1602893794fca4027dda11af344165f2ec
Alex Quach <alexhq1000@gmail.com>
2020-08-25 13:56:56 -0400

    Updates

---
+++ precache-manifest.1ad4e1c30654928eec5461eb825a80aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec75bf0500bdf4a0c91fd36cc85f3d68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5af81b65b22716f96a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e2a7273976c7a35de8c",
+    "url": "/static/js/2.d5968674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5968674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5af81b65b22716f96a",
+    "url": "/static/js/main.64d2d4c6.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
